2180c4caa8fb8a59ecc198a417a5755ccfc3cfa0
pheaktralean <sopheaktralean@gmail.com>
2025-06-02 16:22:43 -0400

    Add signup page with validation requirements

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,35 @@
+/* for the sign up page */
+
+/* get the user tp sign up:
+1 username 
+- does not have to be unique
+- can not be empty
+
+2 email
+- must be a valid email format
+- must be unique
+- can not be empty
+
+3 password 
+- must be at least 8 characters long
+- must contain at least one uppercase letter, one lowercase letter, one number, and one special character
+- can not be empty
+- throw an error if the password does not meet the requirements
+
+4 confirm password
+- must match the password**
+- can not be empty
+- throw an error if the confirm password does not match the password
+
+5 terms and conditions
+- must be checked
+
+6 submit button 
+- should be disabled if any of the above fields are invalid
+- should be enabled if all fields are valid
+- should show a loading spinner while the request is being processed
+// - should show an error message if the request fails
+- should show a success message if the request is successful
+- should redirect the user to the login page after successful sign up
+
+*/
