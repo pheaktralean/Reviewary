097a5f74ded7ed33b5d3c5609edf125bab6c1c2d
pheaktralean <sopheaktralean@gmail.com>
2025-06-02 16:43:34 -0400

    Add folder functionality requirements to documentation

---
+++ client/src/pages/Folders.jsx
@@ -0,0 +1,22 @@
+/* for the folder part
+
+about the folder
+**  search bar should be on the top
+**  folder should be in a grid view
+**  each folder should have a name, color, and actions (delete, archive, favorite, rename, color)
+
+1. user can create a folder
+2. user can delete a folder
+3. user can archice a folder
+4. user can favorite a folder
+5. user can add a folder to a folder
+6. user can rename a folder
+7. user can color a folder (pink, yellow, blue, green, purple,..)
+8. user can add a review card to a folder and remove it 
+9. user can search for a folder by name
+10. user can filter folders by color, favorite, archived
+11. user can sort folders by name, date created, date modified
+12. also there is a trash bin or archive section
+13. user can restore a folder from trash bin or archive section
+
+*/
